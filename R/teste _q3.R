a40f34d493604713d8382fc0625c23455cb93228
Rosenilda <starredrosenilda54@gmail.com>
2021-04-11 21:17:40 -0300

    Initial commit

---
+++ R/teste _q3.R
@@ -0,0 +1,14 @@
+#==============================
+"Script de teste da questão 3"
+"Rosenilda Pereira Barreto"
+"rosenildararidade123@gmail.com"
+#==============================
+
+
+X <- c(68, 70, 72, 58, 90, 110, 68, 70, 72, 80, 80, 67, 90, 94, 100, 80, 75, 79, 84, 90)
+mean(X)
+quantile(X)
+median(X)
+sd(X)
+hist(X)
+
---
+++ R/teste_q4.R
@@ -0,0 +1,13 @@
+#==============================
+"Script de teste da questão 4"
+"Rosenilda Pereira Barreto"
+"rosenildararidade123@gmail.com"
+#==============================
+
+
+
+
+frango_dieta <- read.csv("atividade/dados/brutos/frango_dieta.csv")
+
+mean(frango_dieta$peso)
+sd(frango_dieta$peso)
